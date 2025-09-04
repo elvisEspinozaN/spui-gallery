9d805966a13b394832f8436bbc57d8e3ff11c3d3
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-09-03 20:57:07 -0400

    add initial Sanity setup (schema, config)

---
+++ src/sanity/schemaTypes/artwork.ts
@@ -0,0 +1,43 @@
+import { defineType, defineField } from "sanity";
+
+export const artwork = defineType({
+  name: "artwork",
+  title: "Artwork",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title", maxLength: 96 },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+      fields: [
+        defineField({
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+          description: "Important for SEO and accessibility.",
+          validation: (rule) => rule.required(),
+        }),
+      ],
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    }),
+  ],
+});
